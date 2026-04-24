1eb64404af151b895693ceabae2173631b9ca2ca
gabriellongshaw <gabriel.longshaw@gmail.com>
2026-04-24 10:49:40 +0100

    fix (assets/js/modules/parser.js): improve logic fore removing spaces

--- assets/js/modules/parser.js
+++ assets/js/modules/parser.js
@@ -1,7 +1,53 @@
 export function removeComments(code) {
-    code = code.replace(/\/\*[\s\S]*?\*\//g, '');
-    code = code.replace(/(^|[^:]|[^\\])\/\/.*$/gm, '$1');
-    code = code.replace(/^[ \t]*\n/gm, '');
-    code = code.replace(/\n{2,}/g, '\n');
-    return code;
+    const lines = code.split('\n');
+    const result = [];
+    let inBlock = false;
+    for (let i = 0; i < lines.length; i++) {
+        let line = lines[i];
+        let hadComment = false;
+        let out = '';
+        let j = 0;
+        while (j < line.length) {
+            if (inBlock) {
+                const end = line.indexOf('*/', j);
+                if (end === -1) {
+                    hadComment = true;
+                    j = line.length;
+                } else {
+                    hadComment = true;
+                    j = end + 2;
+                    inBlock = false;
+                }
+            } else {
+                if (line[j] === '/' && line[j + 1] === '*') {
+                    inBlock = true;
+                    hadComment = true;
+                    j += 2;
+                } else if (line[j] === '/' && line[j + 1] === '/' && (j === 0 || line[j - 1] !== ':')) {
+                    hadComment = true;
+                    j = line.length;
+                } else if ((line[j] === '"' || line[j] === "'") ) {
+                    const q = line[j];
+                    out += line[j++];
+                    while (j < line.length && line[j] !== q) {
+                        if (line[j] === '\\') out += line[j++];
+                        out += line[j++];
+                    }
+                    if (j < line.length) out += line[j++];
+                } else {
+                    out += line[j++];
+                }
+            }
+        }
+        if (inBlock && !line.includes('/*')) {
+            hadComment = true;
+            out = '';
+        }
+        const trimmed = out.trim();
+        if (trimmed === '' && hadComment) {
+            continue;
+        }
+        result.push(out);
+    }
+    return result.join('\n');
 }
