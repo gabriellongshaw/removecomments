bc4d28c98bb2cfc478720f87d22f67a3e4103c7c
gabriellongshaw <gabriel.longshaw@gmail.com>
2026-04-24 10:14:49 +0100

    fix (assets/js/modules/parser.js): remove blank lines where comments were

--- assets/js/modules/parser.js
+++ assets/js/modules/parser.js
@@ -1,5 +1,7 @@
 export function removeComments(code) {
     code = code.replace(/\/\*[\s\S]*?\*\//g, '');
     code = code.replace(/(^|[^:]|[^\\])\/\/.*$/gm, '$1');
+    code = code.replace(/^[ \t]*\n/gm, '');
+    code = code.replace(/\n{2,}/g, '\n');
     return code;
 }
